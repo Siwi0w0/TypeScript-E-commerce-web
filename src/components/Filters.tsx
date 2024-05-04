304f2745ebec6cd909939502eb3ad4010f883dcd
Siwi0w0 <yufei.sun64@gmail.com>
2024-05-04 12:59:47 +0100

    update FormSelect component

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,11 +1,10 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+// import { Input } from '@/components/ui/input';
+// import { Label } from '@/components/ui/label';
 import { Button } from './ui/button';
-
 import { type ProductsResponseWithParams } from '@/utils';
-
 import FormInput from './FormInput';
+import FormSelect from './FormSelect';
 
 function Filters() {
   const { meta, params} = useLoaderData() as ProductsResponseWithParams;
@@ -15,6 +14,22 @@ function Filters() {
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-4 items-center'>
       {/* Search */}
       <FormInput type='search' label='search product' name='search' defaultValue={search} />
+
+      {/* CATEGORIES */}
+      <FormSelect
+    label='select category' name='category'
+    options={meta.categories} defaultValue={category} />
+
+      {/* COMPANIES */}
+      <FormSelect
+    label='select company' name='company'
+    options={meta.companies} defaultValue={company} />
+
+      {/* ORDER */}
+      <FormSelect
+          label='order by' name='order'
+          options={['a-z','z-a']} defaultValue={order} />
+
       <Button type='submit' size='sm' className='self-end mb-2'>
         Search
       </Button>
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,21 +1,21 @@
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 type FormInputProps = {
-    name:string;
-    type:string;
-    label?:string;
-    defaultValue?:string
-}
+  name: string;
+  type: string;
+  label?: string;
+  defaultValue?: string;
+};
 
-function FormInput({label, name, type, defaultValue}:FormInputProps) {
+function FormInput({ label, name, type, defaultValue }: FormInputProps) {
   return (
     <div className='mb-2'>
-        <Label htmlFor={name} className='capitalize'>{label || name} </Label>
-        <Input id={name} name={name} type={type} defaultValue={defaultValue
-        }></Input>
+      <Label htmlFor={name} className='capitalize'>
+        {label || name}
+      </Label>
+      <Input id={name} name={name} type={type} defaultValue={defaultValue} />
     </div>
-  )
+  );
 }
-
-export default FormInput
+export default FormInput;
