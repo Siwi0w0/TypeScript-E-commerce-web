279ebfb0dbf2b22c3431bf052f3d81af979b1a69
Siwi0w0 <yufei.sun64@gmail.com>
2024-06-10 20:36:12 +0100

    update single product page - add color selection

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -1,8 +1,7 @@
 import { useLoaderData } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link, type LoaderFunction } from 'react-router-dom';
 import {
   customFetch,
-  formatAsDollars,
   formatAsEuros,
   type SingleProductResponse,
 } from '@/utils';
@@ -10,8 +9,6 @@ import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { SelectProductAmount, SelectProductColor } from '@/components';
-
-import { type LoaderFunction } from 'react-router-dom';
 
 export const loader:LoaderFunction = async({params
 }):Promise<SingleProductResponse> =>{
@@ -24,7 +21,7 @@ function SingleProduct() {
   const {image,title,price,description,colors,company} = product.attributes;
   const eurosAmount = formatAsEuros(price);
 
-  // usestate of colors and amount of a single product
+  // useState of colors and amount of a single product
   const [productColor, setProductColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
@@ -33,7 +30,36 @@ function SingleProduct() {
   }
 
   return (
-    <div>SingleProduct</div>
+    <section>
+      <div className="flex gap-x-2 h-6 items-center">
+        <Button asChild variant = 'link' size='sm'>
+          <Link to='/'>Home</Link>
+        </Button>
+        <Separator orientation='vertical' />
+        <Button>
+          <Link to='/products'>Product</Link>
+        </Button>
+      </div>
+      {/* PRODUCT */}
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
+        {/* Image First Col */}
+        <img src={image} alt={title} className='w-96 h-96 object-cover rounded-lg lg:w-full'/>
+
+        {/* Product info seciton col */}
+        <div>
+          <h1 className='captitalize text-3xl font-bold'>{title}</h1>
+          <h4 className='text-xl mt-2'>{company}</h4>
+          <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>{eurosAmount}</p>
+          <p className='mt-6 leadning-8'>{description}</p>
+          {/* COLORS */}
+          <SelectProductColor colors={colors} productColor={productColor} setProductColor={setProductColor}/>
+          {/* AMOUNT */}
+          <SelectProductAmount/>
+          {/* CART BUTTON */}
+          <Button size='lg' className='mt-10' onClick={addToCart}>Add to cart</Button>
+        </div>
+      </div>
+    </section>
   )
 }
 
